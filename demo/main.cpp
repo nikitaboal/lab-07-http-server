676be34c8ab2af20a792e65a903dc8c5933dfd96
nikitaboal <nikbold-02@yandex.ru>
2022-05-15 20:46:18 +0300

    v1

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,6 @@
-#include <example.hpp>
 
-int main() {
-  example();
+#include "processing_request.hpp"
+
+int main(int argc, char* argv[]) {
+  go_useful_server(argc, argv);
 }
--- tests/example.cpp
+++ tests/example.cpp
@@ -1,11 +1,10 @@
 // Copyright 2021 Your Name <your_email>
 
-#include <stdexcept>
-#include <thread>
-
 #include <gtest/gtest.h>
 
-#include <example.hpp>
+#include <processing_request.hpp>
+#include <stdexcept>
+#include <thread>
 
 TEST(Example, EmptyTest) {
     EXPECT_THROW(example(), std::runtime_error);
